c9df775780806ebdd253a8d252861ee8c1ecc4ff
Julio-Martel <julioramdom123@gmail.com>
2026-03-04 20:04:04 -0300

    se han hecho las primeras practicas con post

--- server.js
+++ server.js
@@ -1,9 +1,36 @@
 const express = require("express");
-
 const aplicacion = express();
+const tareas = require("./tareas/tareas");
+
+aplicacion.use(express.json())
 
 aplicacion.get('/',(req,res) => {
-    res.end('Bienvenido a mi aplicacion para agregar tareas.')
+    res.end('Bienvenido a mi mini app de tareas');
+})
+
+aplicacion.get('/tareas', (req,res) => {
+    res.json(tareas);
+})
+
+aplicacion.post('/tareas', (req,res) => {
+
+    const nuevaTarea = {
+        id: req.body.id,
+        nombreVidejuego: req.body.nombreVidejuego,
+        Edad: req.body.Edad,
+        stock: req.body.stock
+    } 
+
+    tareas.push(nuevaTarea);
+
+    console.log(tareas)
+
+    res.json(nuevaTarea);
+
+})
+
+aplicacion.get('/tareas', (req,res) => {
+    res.end(tareas);
 })
 
 aplicacion.listen(3000, () => {
